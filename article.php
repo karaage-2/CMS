187604347819bea978167078f274bb8948a9b625
karaage-2 <taiyokihara@gmail.com>
2022-07-03 20:35:44 +0900

    vscode-test

--- article.php
+++ article.php
@@ -1,13 +1,16 @@
 <?php
 
-$title = “”;
+$title = "";
 $description = "";
-$thumbnail-url = "";
-$og-summary = "";
+$thumbnailurl = "";
+$ogsummary = "";
+
 
 ?>
 <!-- 全ファイル構造化データに対応させる。 -->
 <?php require 'header.php'; ?>
+
 <body>
 </body>
+
 </html>
