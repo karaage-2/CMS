8004948d91433f0289619573ae91c7a98bb6edbb
karaage-2 <taiyokihara@gmail.com>
2022-07-05 21:08:11 +0900

    settings.phpに設定内容を書き込み。初回。

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+$icon16 = '';
+$icon32 = '';
+$icon48 = '';
+$icon62 = ''; // https://benkyouki.com/icon-62.png
+$appimg = ''; // https://benkyouki.com/icon.png
+$themecolor = ''; //#1c8adb
+$sitename = ''; //my study note
+$author_twitter_account = ''; //@benkyouki-official
+$facebook_app_id = '';
