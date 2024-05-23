c95652450825dbc7789d22a35ef2a9bd9b66459d
karaage-2 <taiyokihara@gmail.com>
2024-05-23 23:09:14 +0900

    aa

--- Generator/Generator.js
+++ Generator/Generator.js
@@ -1,11 +1,28 @@
-const save = document.getElementById("save");
-save.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const save = document.getElementById("save");
+  const editor = new EditorJS({
+    holder: "editor",
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar: ["link"],
+      },
+      list: List,
+      checklist: Checklist,
+      quote: Quote,
+      code: CodeTool,
+    },
+  });
+  save.addEventListener("click", (e) => {
     editor
-        .save()
-        .then((outputData) => {
-            console.log("Article data: ", outputData);
-        })
-        .catch((error) => {
-            console.log("Saving failed: ", error);
-        });
+      .save()
+      .then((outputData) => {
+        let blob = new Blob([JSON.stringify(outputData.blocks)], { type: "application/json" });
+        e.target.download = prompt("The article permalink name?", "article");
+        e.target.href = URL.createObjectURL(blob);
+      })
+      .catch((error) => {
+        alert("Saving failed: ", error);
+      });
+  });
 });
